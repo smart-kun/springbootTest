f6b2cbc63a74dbeafab5ac0fc4ba78e6831be6e9
smart-kun <2423825724@qq.com>
2019-06-15 15:26:59 +0800

    配置父子工程中的pom文件

---
+++ springboot-web/src/main/java/com/seu/springboot/web/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.seu.springboot.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by zangkun on 2019/6/15.
+ */
+@Controller
+public class UserController {
+
+    public String getUserList(){
+        return "zangkun";
+    }
+
+}
